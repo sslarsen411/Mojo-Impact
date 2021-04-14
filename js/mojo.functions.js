1a25d151e3b0805285f1b8cab4b58b9f179773d6
Scott Larsen <sslarsen411@gmail.com>
2021-04-14 09:44:14 -0700

    Add waggle to footer icons

--- js/mojo.functions.js
+++ js/mojo.functions.js
@@ -1,12 +1,29 @@
-/*******  ALL BUTTONS *******/
-const btns = document.querySelectorAll(".btn")
+/** 
+* GLOBAL VALUES
+*/
+const rootEle        = document.documentElement,
+      scrollToTopBtn = document.querySelector(".btnScrollToTop"),
+      header         = document.querySelector("header"),
+      scrollTopConst = 50,
+      ToggleRatio    = 0.20,
+      pgName         = document.getElementsByClassName("pg-name"),
+      navBar         = document.getElementsByClassName("nav-bar__link"),
+      navContainer   = document.querySelector(".nav-bar"),
+      burger         = document.querySelector(".nav-burger"),
+      btns           = document.querySelectorAll(".btn"),
+      modal          = document.getElementsByClassName('modal')[0],
+      btnClose       = document.querySelectorAll(".modal__close"),
+      mdlID          = document.getElementById('policy-modal'),
+      hasHero        = document.getElementsByClassName("hero")
+
+/*******  ALL BUTTONS ******
 for (const butt of btns) {
   butt.addEventListener('click', function(e) {
-    //alert('click');
     e.preventDefault()
     console.log(this.href)
   })
 }
+*/ 
 const noLinks = document.querySelectorAll(".isDisabled")
 for (const noLink of noLinks) {
   noLink.addEventListener('click', function(e) {   
@@ -17,22 +34,20 @@ for (const noLink of noLinks) {
 /** 
 * SCROLL EVENTS
 */
-const rootEle    = document.documentElement,
-      scrollToTopBtn = document.querySelector(".btnScrollToTop"),
-      header         = document.querySelector("header"),
-      scrollTopConst = 50,
-      ToggleRatio    = 0.20
 /* CHANGE HEADER BACKGROUND */
+if(hasHero.length == 0){header.classList.add("bg-black")} // CHANGE ON HERO ONLY
 const colorHeader = () => {
-  if ( rootEle.scrollTop < scrollTopConst) {
-    header.classList.remove("bg-black");
-    header.classList.add("bg-trans");
-  } else {
-    header.classList.remove("bg-trans");
-    header.classList.add("bg-black");
-  }
+  if(hasHero.length > 0){
+    if ( rootEle.scrollTop < scrollTopConst) {
+      header.classList.remove("bg-black")
+      header.classList.add("bg-trans")
+    } else {
+      header.classList.remove("bg-trans")
+      header.classList.add("bg-black")
+    }
+  }  
 }
-/* SCROLL TO TOP BUTTON */
+/* ADD SCROLL TO TOP BUTTON */
 const handleScroll = () => {
   let scrollTotal = rootEle.scrollHeight - rootEle.clientHeight
   if ((rootEle.scrollTop / scrollTotal) > ToggleRatio) {
@@ -47,9 +62,10 @@ const scrollToTop = () => {
     behavior: "smooth"
   })
 }
+/* SCROLL LISTENERS */
 scrollToTopBtn.addEventListener("click", scrollToTop)
 document.addEventListener("scroll", handleScroll)
-window.onscroll = function() {colorHeader()};
+window.onscroll = function() {colorHeader()}
 
 /** 
 * NAVIGATION
@@ -59,39 +75,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
     updateMenu(document.getElementsByClassName("pg-name")[0].innerText,true)
 });
 /* LIVE*/
-const updateMenu = (inPg, isInit=false) =>{    
-  let pgName = document.getElementsByClassName("pg-name"),
-      navBar = document.getElementsByClassName("nav-bar__link");
-
+const updateMenu = (inPg, isInit=false) =>{  
   let currPg = pgName[0].innerText    
   pgName[0].innerText = inPg
   for (let i = 0; i < navBar.length; ++i) {    
-      let linkTitle = navBar[i].getAttribute("href"); 
-
+      let linkTitle = navBar[i].getAttribute("href")
       if (linkTitle.includes(currPg) && !isInit){
           navBar[i].parentNode.classList.remove("is-current-page") 
           navBar[i].classList.remove("is-current-page") 
-
       }else if (linkTitle.includes(pgName[0].innerText)){
           navBar[i].parentNode.classList.add("is-current-page")   
           navBar[i].classList.add("is-current-page")
       }
   }
 }
-/* Handle Nav Burger Click */
-const navContainer = document.querySelector(".nav-bar"),
-      burger = document.querySelector(".nav-burger")
+/* Handle Nav Burger Click */ 
 burger.addEventListener("click", () => {
     burger.classList.toggle("clicked");
-    navContainer.classList.toggle("show-nav-bar");  
+    navContainer.classList.toggle("show-nav-bar")
 });
 
 /** 
 * SITE POLICY MODAL
-*/
-const   modal  = document.getElementsByClassName('modal')[0],
-        btnClose  = document.querySelectorAll(".modal__close"),
-        mdlID =  document.getElementById('policy-modal')
+*/   
 const getModalContent = (inPolicy,inTitle) =>{
     fetch('http://localhost/Mojo-Impact/php_scripts/fetchModalContent.php', {
         method: 'POST',      
@@ -109,19 +115,19 @@ const getModalContent = (inPolicy,inTitle) =>{
 for (const btn of btnClose) {
   btn.addEventListener('click', function(e) {   
     e.preventDefault()
-    closeModal();
+    closeModal()
   })
 }
 window.onclick = function(event) {  //click outside of modal
   if (event.target == mdlID) {
-    closeModal();
+    closeModal()
   }
 }
 const closeModal = () =>{
-  modal.classList.remove('modal--show');
-  modal.classList.add('modal--hide');  // Remove hide class after animation is done
+  modal.classList.remove('modal--show')
+  modal.classList.add('modal--hide')  // Remove hide class after animation is done
   afterAnimation = function() {
-    modal.classList.remove('modal--hide');
+    modal.classList.remove('modal--hide')
   }  
-  modal.addEventListener("animationend", afterAnimation, false);
+  modal.addEventListener("animationend", afterAnimation, false)
 }
